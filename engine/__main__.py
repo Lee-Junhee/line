ac3e7d446d740cfc427df27c2208f15faa5f6a8e
Lee-Junhee <jlee02@stuy.edu>
2020-02-10 10:57:44 -0500

    copied over work from branch

--- engine/__main__.py
+++ engine/__main__.py
@@ -1,17 +1,21 @@
 import math
-from canvas import Picture, Color
+from canvas.classes import Picture, Color
 from line import Line
 
-p = Picture('pic.ppm', 128, 128, 255)
+p = Picture('pic.ppm', 1024, 1024, 256)
 
-c = Color(
-    lambda x, y: 255,
-    lambda x, y: 255,
-    lambda x, y: 255
-    )
+lineColor = p.addcolor(Color(
+    lambda x, y: 0 + (x + y) // 8,
+    lambda x, y: 256 - (x + y) // 8,
+    lambda x, y: abs(256 - (x + y) // 4)
+    ))
 
-p.addcolor(c)
+l = Line(p, lineColor)
 
-Line.draw(p, c, 0, 100, 100, 13)
+for i in range(0, 512, 7):
+    l.draw(i, 511, 511, 511 - i)
+    l.draw(512, i, 512 + i, 511)
+    l.draw(1023 - i, 512, 512, 512 + i)
+    l.draw(512, 1023 - i, 511 - i, 512)
 
 p.commit()
